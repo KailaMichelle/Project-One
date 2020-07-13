334a3ef641d835b3c6c1dfab08e0dd7fae6fcae9
Alpha0mega <alpha0megac0derpl@gmail.com>
2020-07-13 17:57:51 -0400

    basic route

--- controllers/index.js
+++ controllers/index.js
@@ -3,14 +3,18 @@
 const express = require("express");
 const router = express.Router();
 
-// Login / Laniding page Route : GET
 router.get("/", (req, res) => {
+  res.render("index");
+});
+
+//TODO from main page
+// DashBoard Route : GET
+router.get("/login", (req, res) => {
   res.render("login");
 });
 
-// DashBoard Route : GET
-router.get("/dashboard", (req, res) => {
-  res.render("index");
-});
+//TODO user/index.ejs
+//TODO  contacts
+//TODO  /resources
 
 module.exports = router;
