bf6e99e9d330d717f90d057ae897fdbdf12240f8
Alpha0mega <alpha0megac0derpl@gmail.com>
2020-07-13 14:38:10 -0400

    server up

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/.env" });
 const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(
+  PORT,
+  console.log(`Server Running in ${process.env.NODE.ENV} mode on port:${PORT}`)
+);
