9dd6ca0b6dcbde23bf9e55ef5439abad5ce7cc2f
Alpha0mega <alpha0megac0derpl@gmail.com>
2020-07-13 19:51:58 -0400

    google Auth, passport, UserSchema finsihed server,DB operational

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const passport = require("passport");
+const session = require("express-session");
 const connectDb = require("./models/index");
 
+// ! Config's
 dotenv.config({ path: "./config/.env" });
+require("./config/passort")(passport); //argument is the passport we required
+
+//! Server + DB Inits
 const app = express();
 connectDb();
-// ----- Static Path Public -----
-app.use(express.static(__dirname + "/public"));
+
 // ----- Views EJS -----
 app.set("view engine", "ejs");
+//! Session need to be above Passport
+app.use(
+  session({
+    secret: "WebApp",
+    resave: false, // if nothing changed in session don't save
+    saveUninitialized: false, // don't create a session until something is stored
+    //? later a store value will be here to store user in mongo
+  })
+);
+//! Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ----- Static Path Public -----
+app.use(express.static(__dirname + "/public"));
+
 // ----- Routes -----
-// app.get("/", (req, res) => {
-//   res.render("index");
-// });
+app.get("/", (req, res) => {
+  res.render("index");
+});
 app.use("/", require("./controllers/index"));
 
 // PORT
